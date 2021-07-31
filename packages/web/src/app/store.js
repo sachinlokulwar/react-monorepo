453626562acebbc71be0f923dc03e6022027fbe0
taher <taher@entropia.com>
2021-08-01 00:46:52 +0800

    react web working from common

--- packages/web/src/app/store.js
+++ packages/web/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import counterReducer from 'common/src/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
--- packages/web/src/features/counter/Counter.js
+++ packages/web/src/features/counter/Counter.js
@@ -7,7 +7,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
+} from 'common/src/counter/counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
