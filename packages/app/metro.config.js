2b1e9629db3000895dcd8c352931b98b07c89405
taher <taher@entropia.com>
2021-08-01 14:14:07 +0800

    metro path

--- packages/app/metro.config.js
+++ packages/app/metro.config.js
@@ -26,5 +26,24 @@ module.exports = {
       },
     ),
   },
-  watchFolders: [path.resolve(__dirname, '../')],
+  watchFolders: [
+    path.resolve(__dirname, '../'),
+    path.resolve(__dirname, '../../node_modules'),
+  ],
 };
+
+// const path = require('path');
+
+// module.exports = {
+//   transformer: {
+//     getTransformOptions: async () => ({
+//       transform: {experimentalImportSupport: false, inlineRequires: false},
+//     }),
+//   },
+//   watchFolders: [path.resolve(__dirname, '../../node_modules')],
+// };
+
+
+// node_modules
+// ../../node_modules
+// /home/taher/mymonorepo/packages/app/node_modules/react-native-paper
